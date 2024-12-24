couple small fixes, it averages around 24 fps on my computer. Hopefully it can be optimized for at least 30 fps

screen-recorder-buffer.py
@@ -18,7 +18,7 @@ frame_rate = 60
 #TODO FRAME RATE CANNOT BE CONSTANT; FRAMERATE HAS TO BE A VARIABLE OF THE COMPUTERS SPEED!!!!!!!
 buffer_duration = 60
 buffer_size_video = frame_rate * buffer_duration
-CHUNK = 2048
+CHUNK = 4096
 FORMAT = pyaudio.paInt16
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
 frame_buffer = deque(maxlen=buffer_size_video)
@@ -63,41 +63,27 @@ combined_output = "combined.mp3"
 #Resulting file
 final_output = "final.mp4"
 
-stop_threads = False
+global stop_threads
 
 def record_screen():
     print("Recording... Press 'q' to quit or 's' to save the last minute.")
 
     with mss.mss() as sct:
         monitor = sct.monitors[1]
-        screen_width = monitor["width"]
-        screen_height = monitor["height"]
-
-        frame_duration = 1/frame_rate
-        global stop_threads
         while not stop_threads:
-            frame_start = time.perf_counter()
-            img = np.array(sct.grab(monitor))
-            frame_buffer.append(img)
-
-            # Sleep to maintain frame rate
-            elapsed = time.perf_counter() - frame_start
-            time.sleep(max(0.0, frame_duration - elapsed))
+            frame_buffer.append(sct.grab(monitor))
 
 def record_audio_to_buffer():
     print(f"Recording from: ({default_speakers['index']}){default_speakers['name']}")
-    global stop_threads
     while not stop_threads:
         data = system_stream.read(CHUNK)
         buffer_system.append(data)
 
 def record_mic_to_buffer():
-    print(f"Recording from: ({default_speakers['index']}){default_speakers['name']}")
-    global stop_threads
+    print(f"Recording from: ({default_mic['index']}){default_mic['name']}")
     while not stop_threads:
         data = microphone_stream.read(CHUNK)
         mic_buffer.append(data)
-
 
 def save_audio(output_file):
     """Save the audio stored in the circular buffer to a WAV file."""
@@ -134,11 +120,11 @@ def save_mic(output_file):
     print(f"Buffered audio saved to {output_file}")
 
 def convert(img):
-    return cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    return cv2.cvtColor(np.array(img), cv2.COLOR_BGRA2BGR)
 
 
-def save_last_minute(screen_size):
-    print("Saving the last minute...")
+def save_screen():
+    print(f"Saving video to {screen_output}")
 
     # Create a VideoWriter object
     out = cv2.VideoWriter(screen_output, fourcc, frame_rate, screen_size)
@@ -148,9 +134,8 @@ def save_last_minute(screen_size):
     buffer_copy = map(convert, buffer_copy)
     for frame in buffer_copy:
         out.write(frame)
-
     out.release()
-    print(f"Last minute saved to {screen_output}")
+    print(f"Video saved to {screen_output}")
 
 def merge_audio():
     print("Merging system and microphone audio...")
@@ -175,6 +160,7 @@ def update_framerate():
     frame_buffer = deque(frame_buffer, maxlen=buffer_size_video)
 
 if __name__ == '__main__':
+    stop_threads = False
     system_thread = threading.Thread(target=record_audio_to_buffer)
     screen_thread = threading.Thread(target=record_screen)
     mic_thread = threading.Thread(target=record_mic_to_buffer)
@@ -189,18 +175,17 @@ if __name__ == '__main__':
         while not stop_threads:
             if keyboard.is_pressed('p'):
                 print(frame_rate)
-                print(buffer_size_video)
             elif keyboard.is_pressed('s'):
                 save_audio(system_output)
                 save_mic(mic_output)
-                save_last_minute(screen_size)
+                save_screen()
                 merge_audio()
                 merge_video_and_audio()
                 os.remove(system_output)
                 os.remove(screen_output)
                 os.remove(mic_output)
                 os.remove(combined_output)
-            elif keyboard.is_pressed('q'):  # Quit recording
+            elif keyboard.is_pressed('q'):
                 stop_threads = True
 
     finally: